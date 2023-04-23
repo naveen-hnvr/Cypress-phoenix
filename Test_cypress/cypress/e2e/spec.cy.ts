7edd9a189959c9a5a24a0feb20951bfad0f14cbc
Naveen Naik <naveen.naik@bitloka.com>
2023-04-23 11:09:09 +0530

    first commit

---
+++ Test_cypress/cypress/e2e/spec.cy.ts
@@ -0,0 +1,8 @@
+describe('Hello Application test', () => {
+  it('passes', () => {
+    cy.visit('/')
+    cy.get('section.phx-hero h1').should('contain.text', 'Welcome to Phoenix!')
+    cy.get('section.phx-hero p').should('contain.text', 'Peace of mind from prototype to production')
+    cy.get('a:contains(Guides & Docs)').click()
+  })
+})
